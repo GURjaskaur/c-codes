5bd79060090b661a685502b7ff72ba18ad1d518e
GURjaskaur <gurjas2525@gmail.com>
2024-04-04 23:07:11 +0530

    Add fibonaci.cpp

--- power.cpp
+++ power.cpp
@@ -22,12 +22,10 @@
 int power(int m, int n)
 {
     if (n == 0)
-    {
         return 1;
-    }
     if (n % 2 == 0)
         return power(m * m, n / 2);
-    return m * power(m * m, n - 1 / 2);
+    return m * power(m * m, (n - 1) / 2);
 }
 
 int main()
