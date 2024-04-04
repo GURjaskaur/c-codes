feccb90aa510cae9dbf970d9c128c2359eda86b8
GURjaskaur <gurjas2525@gmail.com>
2024-04-04 20:00:09 +0530

    Add power.cpp

--- power.cpp
+++ power.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 // int power(int n, int m)
 // {
 //     int pro = 1;
@@ -11,16 +12,28 @@
 //     return pro;
 // }
 
-int power(int n, int m)
+// int power(int n, int m)
+// {
+//     if (n = 1)
+//         return m;
+//     return power(m, n - 1) * m;
+// }
+
+int power(int m, int n)
 {
-    if (n = 1)
-        return m;
-    return power(m, n - 1) * m;
+    if (n == 0)
+    {
+        return 1;
+    }
+    if (n % 2 == 0)
+        return power(m * m, n / 2);
+    return m * power(m * m, n - 1 / 2);
 }
+
 int main()
 {
     int r;
-    r = power(2, 3);
+    r = power(2, 9);
     printf("%d", r);
     return 0;
 }
