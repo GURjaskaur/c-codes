610b78a39bb2434fcee22c56861a4e4415c33c10
GURJAS KAUR <103319032+GURjaskaur@users.noreply.github.com>
2024-04-04 16:52:33 +0530

    factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,31 @@
 #include <stdio.h>
+// recurion
+
+// int fact(int n)
+// {
+//     if (n == 0)
+//         return 1;
+//     return fact(n - 1) * n;
+// }
+
+// using loops
+
+// int fact(int n)
+// {
+//     int i;
+//     int factt = 1;
+//     if (n == 0 || n < 0)
+//         return 1;
+//     for (i = 2; i < n + 1; i++)
+//     {
+//         factt = factt * i;
+//     }
+//     return factt;
+// }
 int main()
 {
-    printf("hello world!");
+    int a;
+    a = fact(-2);
+    printf("%d", a);
     return 0;
 }
